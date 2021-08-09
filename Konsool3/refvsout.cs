260ef163b82eae288a1cdccea683c43b394ca101
Daniil Voroshnin <88653131+DVOniksWyvern@users.noreply.github.com>
2021-08-09 19:23:44 +0300

    yes

---
+++ Konsool3/refvsout.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RefVsOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 1;
+            int second = 2;
+
+            Method1(out first, out second); // passing both arguments to methods as referency types
+            Method2(first, ref second); // passing second argument by a reference
+            Method3(ref first, second); // the other way, passing first argument by a reference
+
+            Console.WriteLine($"first: {first}, second: {second}"); // the result is 10 and 10 because effectively these both are 2*5, just with a different ways of passing the information
+            Console.ReadLine();
+
+        }
+
+        static void Method1(out int a, out int b)
+        {
+            a = 2;
+            b = 1;
+        }
+
+        static void Method2(int a, ref int b)
+        {
+            a *= 5;
+            b *= a;
+        }
+
+        static void Method3(ref int a, int b)
+        {
+            a *= 5;
+            b *= b;
+        }
+    }
+}
